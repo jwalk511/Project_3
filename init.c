ebcc4bcef1dddba172a012bf2413a33f9cdf5fa7
Joshua <walke333@purdue.edu>
2016-03-08 21:24:08 -0500

    Auto Push

---
+++ init.c
@@ -0,0 +1,17 @@
+//
+//  init.c
+//  
+//
+//  Created by Joshua on 3/8/16.
+//
+//
+
+#include <stdio.h>
+#include "boilerplate.h"
+
+void init() {
+    //Slave C to B
+    nSyncedMotors = synchBC;
+    nSyncedTurnRatio = DRIVE_STRAIGHT;
+    
+}

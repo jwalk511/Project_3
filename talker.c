38232af026bdcdb3d7397203f341a1c1eead68af
Nathaniel Roe <roe13@purdue.edu>
2016-04-10 12:16:49 -0400

    no message

--- talker.c
+++ talker.c
@@ -12,8 +12,8 @@ int getDirection();
 
 static int coordinates[2];
 static int prevCoordinates[2];
-static int[] errorFlags = {1, 2, 4, 8, 16, 32};
-static int[] errors = {0, 0, 0, 0, 0, 0};
+static int errorFlags[] = {1, 2, 4, 8, 16, 32};
+static int errors[] = {0, 0, 0, 0, 0, 0};
 static int prevDirection;
 static int prevVector[2];
 
@@ -21,96 +21,105 @@ static int prevVector[2];
 task main()
 {
 
-sendMessage(0);
-ClearMessage();
+	sendMessage(0);
+	ClearMessage();
 
-while (1) {
-ClearMessage();
-coords(HEIGHT, coord);
-wait1Msec(5000);
-}
+	while (1) {
+		ClearMessage();
+		coords(HEIGHT);
+		wait1Msec(5000);
+	}
 }
 
 void coords(int height) {
-int error;
+	int error;
 
-sendMessage(HEIGHT);
+	sendMessage(HEIGHT);
 
-do {
-error = messageParm[0];
-parseError
-#if DEBUG
-printErrors();
-#endif
+	do {
+		error = messageParm[0];
+		parseError(error);
+	if (DEBUG) {
+		printErrors();
+	}
 
-nxtDisplayBigTextLine(2, "%d", error);
-if (error == 0) {
-nxtDisplayTextLine(2, "Waiting for message...");
-wait1Msec(100);
-} else if (errors[0] || errors[1] || errors[2]) {
-Read((coordinates + 0), (coordinates + 1));
-nxtDisplayBigTextLine(2, "%d, %d", coordinates[0], coordinates[1]);
-int line = 0;
-if (errors[0]) nxtDisplayTextLine(3 + line++, "No Error");
-if (errors[1]) nxtDisplayTextLine(3 + line++, "Manual Override");
-if (errors[2]) nxtDisplayTextLine(3 + line++, "Out of Bounds");
-} else {
-int line = 0;
-if (errors[3]) nxtDisplayTextLine(3 + line++, "No ALV Marker Seen");
-if (errors[4]) nxtDisplayTextLine(3 + line++, "LSTS System Error");
-if (errors[5]) nxtDisplayTextLine(3 + line++, "Busy-Request again later");
-}
-} while (error == 0);
+		nxtDisplayBigTextLine(2, "%d", error);
+		if (error == 0) {
+			nxtDisplayTextLine(2, "Waiting for message...");
+			wait1Msec(100);
+			} else if (errors[0] || errors[1] || errors[2]) {
+			Read(&(coordinates[0]), &(coordinates[1]));
+			nxtDisplayBigTextLine(2, "%d, %d", coordinates[0], coordinates[1]);
+			int line = 0;
+			if (errors[0]) nxtDisplayTextLine(3 + line++, "No Error");
+			if (errors[1]) nxtDisplayTextLine(3 + line++, "Manual Override");
+			if (errors[2]) nxtDisplayTextLine(3 + line++, "Out of Bounds");
+			} else {
+			int line = 0;
+			if (errors[3]) nxtDisplayTextLine(3 + line++, "No ALV Marker Seen");
+			if (errors[4]) nxtDisplayTextLine(3 + line++, "LSTS System Error");
+			if (errors[5]) nxtDisplayTextLine(3 + line++, "Busy-Request again later");
+		}
+	} while (error == 0);
 }
 
 void Read(int* x, int* y) {
-*x = messageParm[1];
-*y = messageParm[2];
-ClearMessage();
+	*x = messageParm[1];
+	*y = messageParm[2];
+	ClearMessage();
 }
 
 void parseError(int error) {
-int i = 0;
-errors[i] = (error & errorFlags[i]) << i; i++;
-errors[i] = (error & errorFlags[i]) << i; i++;
-errors[i] = (error & errorFlags[i]) << i; i++;
-errors[i] = (error & errorFlags[i]) << i; i++;
-errors[i] = (error & errorFlags[i]) << i; i++;
-errors[i] = (error & errorFlags[i]) << i; i++;
+	int i = 0;
+	errors[i] = (error & errorFlags[i]) << i; i++;
+	errors[i] = (error & errorFlags[i]) << i; i++;
+	errors[i] = (error & errorFlags[i]) << i; i++;
+	errors[i] = (error & errorFlags[i]) << i; i++;
+	errors[i] = (error & errorFlags[i]) << i; i++;
+	errors[i] = (error & errorFlags[i]) << i; i++;
 }
 
 void printErrors() {
-int i = 0;
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
-wait1Msec(1000);
+	int i = 0;
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	nxtDisplayTextLine(i, "Error %d = %d", errors[i++]);
+	wait1Msec(1000);
 }
 
 int * getPos() {
-return coordinates
+	return coordinates;
 }
 
 void getDirectionVector(int* x, int* y) {
-*x = coordinates[0] - prevCoordinates[0];
-*y = coordinates[0] - prevCoordinates[0];
+	int delX = coordinates[0] - prevCoordinates[0];
+	int delY = coordinates[0] - prevCoordinates[0];
+	if (delX || delY) {
+		prevVector[0] = delX;
+		prevVector[1] = delY;
+		*x = delX;
+		*y = delY;
+	} else {
+		*x = prevVector[0];
+		*y = prevVector[1];
+	}
 }
 
 int getDirection() {
-int delX, delY;
-getDirectionVector(&delX, &delY);
+	int delX, delY;
+	getDirectionVector(&delX, &delY);
 
-if (delX == 0) {
-if (delY == 0) return prevDirection;
-else if (delY > 0) prevDirection = 90;
-else if (delY < 0) prevDirection = 270;
-} else if(delY = 0) {
-if (delX > 0) prevDirection = 0;
-else if (delX < 0) prevDirection = 270;
-}
+	if (delX == 0) {
+		if (delY == 0) return prevDirection;
+		else if (delY > 0) prevDirection = 90;
+		else if (delY < 0) prevDirection = 270;
+		} else if(delY == 0) {
+		if (delX > 0) prevDirection = 0;
+		else if (delX < 0) prevDirection = 270;
+	}
 
-return (int)atan((float)delY) / delX);
+	return atan(1.0*delY / delX);
 }

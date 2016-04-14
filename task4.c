78eadcba7e8e0c271aaf4fe29f142631b0808d78
Trang Dieu <DieuT15@pal-nat186-129-5.itap.purdue.edu>
2016-04-14 11:36:51 -0400

    task $

--- task4.c
+++ task4.c
@@ -1,4 +1,4 @@
-//#pragma config(Sensor, S1, Magnetic, sensorAnalog)
+#pragma config(Sensor, S1, Magnetic, sensorAnalog)
 #include "init.h"
 
 void findBeacon();
@@ -10,23 +10,23 @@ task main() {
 
     // Find beacons
 void findBeacon () {
-    while (sensorValue(Magnetic) <= 900 {
+    while (sensorValue(Magnetic) <= 900) {
         driveStraight (3);
     }
-        
+
     //ALV beeps 3 times
     nVolume = 2;
     PlaySound (soundBeepBeep);
     PlaySound (soundBeepBeep);
     PlaySound (soundBeepBeep);
-    
-    //Drop bins    
+
+    //Drop bins
     dropBins();
     wait1MSec (1000);
-    
+
     //Back up to original position
     nMotorEncoderTarget[motorB] = getSLEncoderCount(-50);
     motor[motorB] = -100;
- 
+
     waitUntilStopped(motorB);
 }

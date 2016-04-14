b1450c3d9b6d14fa5a7fb3ef1fbd4dc7b46e097a
Trang Dieu <DieuT15@pal-nat186-132-36.itap.purdue.edu>
2016-04-13 22:50:09 -0400

    Auto Push

--- task4.c
+++ task4.c
@@ -1,7 +1,26 @@
-#include "moveforward.h"
+//#pragma config(Sensor, S1, Magnetic, sensorAnalog)
+#include "init.h"
 
-task main()
-{
+task main() {
+    void driveStraight (50);
 
-
+    // Find beacons
+    void findBeacon () {
+        while (sensorValue(Magnetic) != 1000 {
+            driveStraight (5);
+        }
+        
+        // ALV stops
+        motor[motorB] = 0;
+        motor[motorC] = 0;
+        
+        //ALV beeps 3 times
+        PlaySound (soundBeepBeep);
+        PlaySound (soundBeepBeep);
+        PlaySound (soundBeepBeep);
+        nVolume = 2;
+        wait1MSec (1000);
+    }
 }
+
+    

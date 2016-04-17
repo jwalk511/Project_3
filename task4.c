610d22571b6ebac6e626677eecd2b2ada0f3602f
Trang Dieu <DieuT15@pal-nat186-143-244.itap.purdue.edu>
2016-04-17 11:46:40 -0400

    Auto commit before pull

--- task4.c
+++ task4.c
@@ -1,34 +1,79 @@
 #pragma config(Sensor, S1, Magnetic, sensorAnalog)
 #include "init.h"
-#define DIST_TO_SEARCH 2
-void findBeacon();
-#define MAGNET_VALUE = 483
+#define DIST_TO_SEARCH 1
+#define MAGNET_VALUE 483
+#define MAX_DRIVE 10
 
+void findBeacon();
+void driveUntilBeacon(int maxDrive, int distToSearch);
 
 task main() {
-	init();
-    driveStraight (50); //Drive straight for 50 cm
-    findBeacon ();
+  init();
+  findBeacon ();
 }
+
 
 // Find beacons
 void findBeacon () {
-	
-    while (sensorValue(Magnetic) <= MAGNET_VALUE) { 
-    
-        //Drive straight for 2 cm until magnetic beacon is found
-        driveStraight (DIST_TO_SEARCH); 
-        nxtDisplayTextLine(3, "magsensor= %d", sensorValue(Magnetic));
+	bool foundBeacon = FALSE;
+
+    driveUntilBeacon (MAX_DRIVE, DIST_TO_SEARCH);
+
+    //If ALV couldn't find beacon, go back once more
+    if (foundBeacon = FALSE) {
+    	driveUntilBeacon (-MAX_DRIVE, - DIST_TO_SEARCH);
+
+    	//Go forward to find beacon once more if still couldn't find it
+    	if (foundBeacon = FALSE) {
+    		driveUntilBeacon (MAX_DRIVE, DIST_TO_SEARCH);
+    	}
     }
 
-    //ALV beeps 3 times
-    nVolume = 2;
-    PlaySound (soundBeepBeep);
-    PlaySound (soundBeepBeep);
-    PlaySound (soundBeepBeep);
+    else {
 
-    //Drop bins
-    dropBins();
-    wait1Msec (1000);
-    
+    //ALV beeps 3 times and drop antennas off
+        nVolume = 2;
+        PlaySound (soundBeepBeep);
+        PlaySound (soundBeepBeep);
+        PlaySound (soundBeepBeep);
+
+   	    dropBins();
+   	    wait1Msec (1000);
+    }
+}
+
+
+
+void driveUntilBeacon (int maxDrive, int distToSearch) {
+    bool foundBeacon = FALSE;
+
+    //Reset encoder count to zero
+    resetCount (motorB);
+
+    //Motor B and C move at the same time
+    nSyncedTurnRatio = DRIVE_STRAIGHT;
+
+    nMotorEncoderTarget[motorB] = getSLEncoderCount(maxDrive);
+
+    while (sensorValue(Magnetic) < MAGNET_VALUE) {
+    	motor[motorB] = 30 * sgn(distToSearch);
+
+    	if (abs(nMotorEncoder[motorB]) > abs(nMotorEncoderTarget[motorB])) {
+    		break;
+    	}
+
+    }
+
+    if (sensorValue(Magnetic) < MAGNET_VALUE) {
+        foundBeacon = FALSE;
+    }
+    else {
+        foundBeacon = TRUE;
+    }
+
+    motor[motorB] = 0;
+    nMotorEncoderTarget[motorB] = 0;
+    waitUntilStopped (motorB);
+    resetCount (motorB);
+
 }
